2b0ea54b316416bf8a8b8348692afee6cb4113aa
SchreinerK <SchreinerK@users.noreply.github.com>
2024-08-06 00:53:42 +0200

    .

--- src/init.ps1
+++ src/init.ps1
@@ -1,5 +1,5 @@
 $modulesUrl = "https://raw.githubusercontent.com/KsWare/KsWare.AppVeyorBuildTools/develop/src"
 $modulesPath = ($env:PSModulePath -split ';' | Where-Object { $_ -like "$env:USERPROFILE\*" })[0]   
 Invoke-WebRequest -Uri "$modulesUrl/init-module.psm1" -OutFile  (Join-Path $modulesPath "init-module.psm1")
-Import-Module (Join-Path $modulesPath "init-module.psm1") -Force -Scope Global DisableNameChecking -ErrorAction Stop
+Import-Module (Join-Path $modulesPath "init-module.psm1") -Force -Scope Global -DisableNameChecking -ErrorAction Stop
 Import-AppVeyorModules -baseUrl $modulesUrl -destinationDir $modulesPath -Verbose
